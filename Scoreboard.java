94ba4cd527090e2820853fe8fcec70ad06d3c0e1
Paul Jonas Dohle <paul.jonas@dohle-net.de>
2026-06-10 12:20:20 +0200

    fat: add scoreboard class and its image

---
+++ Scoreboard.java
@@ -0,0 +1,17 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The scoreboard displays essential information in the game,
+ * the points and munition.
+ * 
+ * @author Paul Jonas Dohle
+ * @version 0.1.0
+ */
+public class Scoreboard extends Actor {
+    /** The act method is the acces point for the game loop
+     * and is called regularly. It calls other methods regularly
+     * including the update methode.
+     */
+    public void act() {
+    }
+}
