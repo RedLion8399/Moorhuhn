e46dd82f2778b398bcbe03e83096963aaa4649c4
Paul Jonas Dohle <paul.jonas@dohle-net.de>
2026-06-16 14:26:21 +0200

    feat: add Tree class and its basic structure

---
+++ Tree.java
@@ -0,0 +1,17 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The tree functions as unmovable obstacle chickens can hide behinde in order to avoiid being hit.
+ * 
+ * @author Paul Jonas Dohle
+ * @version 0.1.0
+ */
+public class Tree extends Actor {
+
+    /**
+     * Constructs objects of class tree
+     */
+    public Tree() {
+
+    }
+}
