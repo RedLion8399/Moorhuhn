5b730efbd88ae619ed231ccbf2d09658b576b9d5
Paul Jonas <Paul.Jonas@dohle-net.de>
2026-06-20 20:26:50 +0200

    feat add a Backgroundimage class

---
+++ BackgroundImage.java
@@ -0,0 +1,18 @@
+import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The BackgroundImage class represents the background image in the game.
+ * As the Backround has to move a seperate actor is needed to represet it.
+ * 
+ * @author Paul Jonas Dohle
+ * @version 0.1.0
+ */
+public class BackgroundImage extends Actor {
+    /**
+     * Creates a new BackgroundImage object.
+     * 
+     * @param world the world the actor is in
+     */
+    public BackgroundImage(GameWorld world) {
+    }
+}
